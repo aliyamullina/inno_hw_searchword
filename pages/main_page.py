539990e12ef060ad4897160897e51e73a7eb8670
Aliya Mullina <aliya.mullina@ya.ru>
2021-08-20 18:56:01 +0300

    init

---
+++ pages/main_page.py
@@ -0,0 +1,14 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    SEARCH_INPUT = (By.NAME, "q")
+
+
+class MainPage(BasePage):
+
+    def search(self, text: str):
+        element = self.input(MainPageLocators.SEARCH_INPUT, text)
+        self.click_enter(element)
+        
